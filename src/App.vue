3b5e0e4841272d603fd5d1086fa978c90b92a592
Gabriel Nascimento <gabriel.nascimento@grupointegrado.br>
2023-11-18 18:17:04 -0300

    Header e importação de fontes

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,54 @@
 <template>
-  <HelloWorld msg="Hello World"/>
+  <header>
+    <nav class="navbar">
+      <div class="container-fluid d-flex">
+        <a href="">
+          <img src="../public/LogoON.svg" alt="Logo ON">
+        </a>
+        
+        
+        <button type="button" class="btn btn-outline-light rounded-pill ">
+          Acessar Conta
+        </button>
+        
+      </div>
+    </nav>
+  </header>
+  
+
+
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  /*------- Import de Fontes -------*/
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+
+  /*------- Variáveis de Cores -------*/
+  :root {
+    --azul-claro: #1586CB;
+    --azul-escuro: #105D8E;
+    --preto: #000000;
+    --branco:#FFFFFF;
+    --cinza-claro:#D9D9D9;
+    --cinza-escuro: #6C757D;
+    --verde: #1BC35E;
+    --amarelo: #FFE600;
+  }
+
+  header {
+    background-color: var(--azul-claro);
+    font-family: 'Poppins';
+  }
+
+  
+
+
+
 </style>
