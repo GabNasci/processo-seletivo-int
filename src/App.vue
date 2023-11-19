5f000e3604a30a737c91d2e8de5502358eb09e2a
Gabriel Nascimento <gabriel.nascimento@grupointegrado.br>
2023-11-19 01:04:26 -0300

    Banner com busca e ícones fontawesome

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,53 @@
       </div>
     </nav>
   </header>
+  <main>
+    <section class="busca">
+            <div class=" container justify-content-center align-items-center py-5">
+
+                <div class="d-flex justify-content-center align-items-center">
+                    <h2 class="fs-5">Já sabe o que está procurando?</h2>
+                </div>
+
+                <div class=" d-flex border rounded-pill ">
+                    <div class="border-end rounded-start-pill px-3 py-1">
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn p-0 text-light" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                          <font-awesome-icon icon="fa-solid fa-list" />
+                        </button>
+                        
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                ...
+                                </div>
+                                <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="d-flex justify-content-between w-100">
+                        <input type="text" name="search" id="search" class="busca_input border-0 w-100 px-2" placeholder="administração...">
+                        <button type="submit" class="btn p-0 text-light">
+                          <font-awesome-icon icon="fa-solid fa-magnifying-glass" rotation=90 class="p-2" />
+                        </button>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
+  </main>
   
   
 
@@ -42,12 +89,25 @@
     --cinza-escuro: #6C757D;
     --verde: #1BC35E;
     --amarelo: #FFE600;
+    --fonte-principal: 'Poppins'
   }
 
   header {
     background-color: var(--azul-claro);
-    font-family: 'Poppins';
+    font-family: var(--fonte-principal);
   }
+
+  .busca {
+    color: var(--branco);
+    background: linear-gradient(82deg, #164C6E 7.05%, #105D8E 52.51%, #184F73 96.27%);
+ }
+
+ .busca_input {
+    background: transparent;
+    outline: none;
+    color: var(--branco);
+    
+ }
 
   
 
--- src/main.js
+++ src/main.js
@@ -12,9 +12,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faList, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faMagnifyingGlass, )
+library.add(faMagnifyingGlass, faList)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(bootstrap).mount('#app')
